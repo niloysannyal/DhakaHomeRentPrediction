4019d51c3ded8db93e7a86220bc82f224115bbfa
niloysannyal <61078546+niloysannyal@users.noreply.github.com>
2025-04-17 01:56:17 +0600

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask import send_from_directory
-from server import util
+import util
 import os
 
 client_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../client'))
