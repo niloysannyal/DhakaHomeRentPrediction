dde1393660910e5c3de226246e20e8df3ac29bb8
niloysannyal <61078546+niloysannyal@users.noreply.github.com>
2025-04-19 15:37:32 +0600

    Update app.js

--- client/app.js
+++ client/app.js
@@ -42,19 +42,22 @@ function onClickedEstimatePrice() {
 }
 
 function onPageLoad() {
-  console.log( "document loaded" );
-  var url = "https://dhakahomerentprediction.onrender.com/get_location_names";
-  $.get(url,function(data, status) {
-      console.log("got response for get_location_names request");
-      if(data) {
-          var locations = data.Locations;
-          var uiLocations = document.getElementById("uiLocations");
-          $('#uiLocations').empty();
-          for(var i in locations) {
-              var opt = new Option(locations[i]);
-              $('#uiLocations').append(opt);
-          }
+  console.log("document loaded");
+  var url = "https://dhakahomerentprediction.onrender.com/predict_home_rent";
+  $.get(url, function (data, status) {
+    console.log("got response for get_location_names request");
+    if (data) {
+      var locations = data.Locations;
+      var uiLocations = document.getElementById("uiLocations");
+      $('#uiLocations').empty();
+      for (var i in locations) {
+        var opt = new Option(locations[i]);
+        $('#uiLocations').append(opt);
       }
+    }
+    $('#uiLocations').select2({
+      width: '100%'
+    });
   });
 }
 
